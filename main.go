2730d5ea92482697c6b267f757688163d37c3224
Edson Moreno <edsonmorenoruiz@gmail.com>
2019-10-09 19:34:43 -0500

    +splitDescription Func, Description is displayed

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 type Sources struct {
@@ -29,6 +30,11 @@ type News struct {
 	Articles     []Articles
 }
 
+func splitDescription(d string) string {
+	sliceOfD := strings.Split(d, " ")
+	return strings.Join(sliceOfD[:25], " ")
+}
+
 func main() {
 	response, err := http.Get("https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=565555cabbae4989934af419afac7973")
 	if err != nil {
@@ -45,11 +51,20 @@ func main() {
 		fmt.Printf("Total Results = %v", news.TotalResults)
 		fmt.Println()
 
-		for x := 0; x < len(news.Articles); x++ {
+		for x := 0; x < len(news.Articles); x++ { // information of all articles provided by API is displayed
 			fmt.Println("Article", x+1)
 			fmt.Println("Source{ Id:", news.Articles[x].Source.Id, "	Name:", news.Articles[x].Source.Name, "}")
 			fmt.Println("Author:", news.Articles[x].Author)
 			fmt.Println("Title:", news.Articles[x].Title)
+			
+			description := news.Articles[x].Description
+			if len(strings.Split(news.Articles[x].Description, " ")) > 25 { // If the Description has more than 25 words...
+				description = splitDescription(news.Articles[x].Description)
+				fmt.Println("Description:", description, "...")
+			} else {
+				fmt.Println("Description:", description)
+			}
+			
 			fmt.Println("")
 		}
 	}
